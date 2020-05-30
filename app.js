a9b5d9e68920dfb23930fa9d36b1c71cfb076443
Naveen Gaur <naveensgour@gmail.com>
2020-05-30 20:06:48 +0530

    Data format is changed according to database friendly manner.

--- app.js
+++ app.js
@@ -10,49 +10,227 @@ app.get("/",function(req,res){
     res.render("index");
 });
 
-
-
-var data=[
+var pageRenderingData=null;
+var faculty=[
+    {
+        id:1,
+        password:1,   
+        name:"Dr. Naveen Kumar",
+        code:[101,210]
+    },
+    {
+        id:2,
+        password:2,
+        name:"Ajay Nath",
+        code:[100,101]
+    }
+];
+var students=[
+    {
+        id:201852022,
+        name:"Naveen Gaur",
+        course:[
+            {
+                code:100,
+                attendance:[
+                    "p","","","","","","",""
+                ]
+            },
+            {
+                code:101,
+                attendance:[
+                    "","p","","","","p","","p"
+                ]
+            }
+        ]
+    },
+    {
+        id:201852019,
+        name:"Mansi Gupta",
+        course:[
+            {
+                code:100,
+                attendance:[
+                    "","p","","","","p","","p"
+                ]
+            },
+            {
+                code:210,
+                attendance:[
+                    "p","","p","","","p","",""
+                ]
+            }
+        ]
+    },
+    {
+        id:201852008,
+        name:"Faizan",
+        course:[
+            {
+                code:210,
+                attendance:[
+                    "","","p","","","","p",""
+                ]
+            }
+        ]
+    },
+    {
+        id:201852005,
+        name:"AP",
+        course:[
+            {
+                code:100,
+                attendance:[
+                    "","p","","","p","","",""
+                ]
+            },
+            {
+                code:101,
+                attendance:[
+                    "","p","","","p","","",""
+                ]
+            },
+            {
+                code:210,
+                attendance:[
+                    "p","","p","","","","","p"
+                ]
+            }
+        ]
+    },
+    {
+        id:201852023,
+        name:"Het Patel",
+        course:[
+            {
+                code:100,
+                attendance:[
+                    "p","p","p","p","p","p","p","p"
+                ]
+            },
+            {
+                code:101,
+                attendance:[
+                    "","","","","","","",""
+                ]
+            },
+            {
+                code:210,
+                attendance:[
+                    "","p","","p","p","","p",""
+                ]
+            }
+        ]
+    },
+    {
+        id:201852024,
+        name:"Prachi Desai",
+        course:[
+            {
+                code:100,
+                attendance:[
+                    "p","","p","","p","","","p"
+                ]
+            },
+            {
+                code:101,
+                attendance:[
+                    "","p","","","","p","","p"
+                ]
+            },
+            {
+                code:210,
+                attendance:[
+                    "p","","","p","","","p",""
+                ]
+            }
+        ]
+    }
+];
+var courses=[
     {
         code:100,
         name:"Physics",
-        attendance:[
-            {id:201852022,name:"Narayan",date:"22:5:19",a:"p"},
-            {id:201852022,name:"Nova",date:"24:5:19",a:"p"},
-            {id:201852022,name:"Nadeen",date:"25:5:19",a:"p"},
-            {id:201852022,name:"Nxt",date:"27:5:19",a:"p"},
-            {id:201852022,name:"Naveen",date:"21:5:19",a:"p"},
+        schedule:[
+            "30:4:19","2:5:19","20:5:19","21:5:19","22:5:19","24:5:19","25:5:19","27:5:19"
+        ],
+        idStudent:[
+            201852022,201852019,201852005,201852023,201852024
         ]
     },
     {
         code:101,
-        name:"Math",
-        attendance:[
-            {id:201852022,name:"Nexsus",date:"20:5:19",a:"p"},
-            {id:201852022,name:"Naveen",date:"2:5:19",a:"p"},
-            {id:201852022,name:"Nova",date:"24:5:19",a:"p"}
+        name:"Maths",
+        schedule:[
+            "2:5:19","20:5:19","21:5:19","22:5:19","24:5:19","25:5:19","27:5:19","1:6:19"
+        ],
+        idStudent:[
+            201852022,201852005,201852023,201852024
         ]
     },
     {
         code:210,
-        name:"AI and Boom",
-        attendance:[
-            {id:201852022,name:"Nxt",date:"20:5:19",a:"p"},
-            {id:201852022,name:"Novas",date:"24:5:19",a:"p"},
-            {id:201852022,name:"Naveen",date:"2:5:19",a:"p"}
+        name:"AI and ML",
+        schedule:[
+            "2:5:19","20:5:19","21:5:19","22:5:19","24:5:19","25:5:19","27:5:19"
+        ],
+        idStudent:[
+            201852019,201852008,201852005,201852023,201852024
         ]
     }
+
 ];
 
-var date=[
-    "2:5:19","20:5:19","21:5:19","22:5:19","24:5:19","25:5:19","27:5:19"
-];
 
 app.get("/faculty",(req,res)=>{
-    res.render("faculty",{data:data,date:date});
-
+    if(pageRenderingData!=null)
+        res.render("faculty",pageRenderingData);
+    else
+        res.redirect("/");
+    pageRenderingData=null;
 });
 
+app.post("/faculty",(req,res)=>{
+    var facultyCourses=[];
+    var attendances=[];
+    faculty.forEach(i => {
+        if(i.id==req.body.user && i.password==req.body.pass){
+            i.code.forEach(code => {
+                var attend=[];
+                var find = courses.find(function(course) {
+                    if(course.code == code)
+                        return true;
+                });
+                find.idStudent=find.idStudent.sort();                
+                facultyCourses.push(find);
+                find.idStudent.forEach(id => {
+                    var student = students.find(function(s) {
+                        if(s.id == id)
+                            return true;
+                    });
+                    var attendanceObj = student.course.find(function(c) {
+                        if(c.code == find.code)
+                            return true;
+                    });
+
+                    attend.push({
+                        id:student.id,
+                        name:student.name,
+                        attendance:attendanceObj.attendance                        
+                    });
+
+                });
+                attendances.push(attend);
+
+            });
+            
+            pageRenderingData={courses:facultyCourses,name:i.name,attendances:attendances};
+            res.redirect("/faculty")
+        }
+
+    });
+    
+});
 
 app.listen(3000,()=>{
     console.log("Server is started on port 3000");
